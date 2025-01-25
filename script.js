4e53c914bd58e1608a317593847d9391edd50dc3
Vivesh kumar <vivesh103@gmail.com>
2025-01-25 22:51:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function stringChop(str, size) {
   // your code here
+	 if (!str) {
+        return [];
+    }
+
+    // Initialize an array to hold the chunks
+    const chunks = [];
+
+    // Loop through the string in increments of chunkLength
+    for (let i = 0; i < str.length; i += size) {
+        // Extract a substring of chunkLength characters and push to the array
+        chunks.push(str.slice(i, i + size));
+    }
+
+    return chunks;
 }
 
 // Do not change the code below
